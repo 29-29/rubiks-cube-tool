6dd227ae3eae3af69de535e7ea5d18b901d1ccd1
29-29 <ackx29sodo@gmail.com>
2023-12-19 14:59:55 +0800

    UPDATED: Refactored stats recording in oll.py and stats.py
    - Reorganized stats tracking in 'oll.py' to use dictionaries and separate encounters count from repetitions
    - Updated 'stats.py' to accurately count encounters and repetitions in 'OLL_STATS' dictionary

--- stats.py
+++ stats.py
@@ -8,14 +8,12 @@ with open("data.json", "r") as f:
 		ALL_STATS = json.loads(s)
 
 OLL_STATS = ALL_STATS["OLL"]
-OLL_count = 0
-for case in OLL_STATS:
+OLL_count = OLL_STATS["count"]
+for case in OLL_STATS["encounters"]:
 	OLL_count += case["reps"]
-# print("Total OLL encounters:", OLL_count)
-
 
 def stats_oll():
-	oll_stats = [(case["reps"],case["str"]) for case in OLL_STATS]
+	oll_stats = [(case["reps"],case["str"]) for case in OLL_STATS["encounters"]]
 	for case in sorted(oll_stats,reverse=True):
 		percent = round(case[0]/OLL_count * 100, 2)
 		print(f"{percent}%", f"({case[0]}/{OLL_count})")
